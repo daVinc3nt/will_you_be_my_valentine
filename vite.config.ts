1428920502ef5adad6982633e77ca8becd82ab2c
QuangTran013 <113117168+daVinc3nt@users.noreply.github.com>
2024-06-19 11:24:17 +0700

    ok

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/will-you-be-my-valentine/", // YOUR REPO NAME HERE
+  base: "/", // YOUR REPO NAME HERE
 });
